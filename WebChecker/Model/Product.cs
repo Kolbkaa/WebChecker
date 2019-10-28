7962965ba368f8ddb968d79e31107be4eca3ee6a
Dawid Kolbusz <dawid.kolbusz1@gmail.com>
2019-10-28 22:35:53 +0100

    add ctor to product

--- WebChecker/Model/Product.cs
+++ WebChecker/Model/Product.cs
@@ -2,6 +2,11 @@
 {
     internal class Product
     {
+        public Product(string name, string price)
+        {
+            Name = name;
+            Price = price;
+        }
         public string Name { get; private set; }
         public string Price { get; private set; }
     }
