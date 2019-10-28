7d7b5f9fc3c13ac441664486a0545eff61437d17
Dawid Kolbusz <dawid.kolbusz1@gmail.com>
2019-10-29 00:06:04 +0100

    Add check method

--- WebChecker/Model/PageToCheck.cs
+++ WebChecker/Model/PageToCheck.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,8 @@ namespace WebChecker.Model
         private readonly string _webNameProductPosition;
         private readonly string _webPriceProductPosition;
         private Dictionary<string, Product> _productDictrionary;
+        private Queue<string> _linkToCheck;
+        private List<string> _linkChecked;
         public PageToCheck(string webURL, string nameProductPostion, string priceProductPosition)
         {
             _webURL = webURL;
@@ -19,6 +22,34 @@ namespace WebChecker.Model
             _webPriceProductPosition = priceProductPosition;
 
             _productDictrionary = new Dictionary<string, Product>();
+            _linkChecked = new List<string>();
+            _linkToCheck = new Queue<string>();
+
+            _linkToCheck.Enqueue(webURL);
+        }
+
+        public void Check()
+        {
+            do
+            {
+                var link = _linkToCheck.Dequeue();
+                if (!_linkChecked.Contains(link))
+                {
+                    WebCheck webCheck = new WebCheck(link);
+                    var LinkList = webCheck.FindLinkOnWeb();
+                    //dopisać metodę która będzie obrabiać linki
+                    foreach (var l in LinkList)
+                    {
+                        if (!_linkChecked.Contains(l))
+                        {
+                            _linkToCheck.Enqueue(l);
+                        }
+                    }
+                    var product = webCheck.FindProduct(_webNameProductPosition, _webPriceProductPosition);
+                    if (product != null) _productDictrionary.Add(link, product);
+                }
+                Debug.WriteLine(_linkToCheck.Count);
+            } while (_linkToCheck.Count > 0);
         }
 
     }
