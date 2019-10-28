f3f9da63838155cdeba14640cf29975fa8d37d6f
Dawid Kolbusz <dawid.kolbusz1@gmail.com>
2019-10-28 22:52:56 +0100

    add price parser

--- WebChecker/Model/Product.cs
+++ WebChecker/Model/Product.cs
@@ -1,13 +1,24 @@
-﻿namespace WebChecker.Model
+﻿using System.Linq;
+
+namespace WebChecker.Model
 {
     internal class Product
     {
+        private string price;
+
         public Product(string name, string price)
         {
             Name = name;
             Price = price;
         }
         public string Name { get; private set; }
-        public string Price { get; private set; }
+        public string Price
+        {
+            get => price;
+            private set
+            {
+                price = new string(value.Where(x=> (char.IsDigit(x) || x == ',' || x=='.')).ToArray());
+            }
+        }
     }
 }
